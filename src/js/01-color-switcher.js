d03d63595b49a231741426b9ff72536b818f9236
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-05-08 16:58:39 +0300

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+
+const refs = {
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
+
+refs.buttonStart.addEventListener('click', changeColor);
+refs.buttonStop.addEventListener('click', stopchangeColor);
+
+let timerColor = null;
+
+function changeColor(){
+    timerColor = setInterval(() => {
+        const randomHexColor = getRandomHexColor();
+        refs.body.style.backgroundColor = randomHexColor;
+        
+}, 2000);
+refs.buttonStart.disabled = true;
+};
+
+function stopchangeColor(){
+    clearInterval(timerColor);
+    refs.buttonStart.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
